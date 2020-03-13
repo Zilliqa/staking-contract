63f4cdd00eb8379427a4dfe23d61146a89be773f
xiaohuo <xiaohuo200@gmail.com>
2020-03-13 19:55:54 +0800

    feat: develop more script

---
+++ scripts/update_minstake.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+zli contract call -a 09710e00256db2e3db4b44f597f17f3d97f06318 -t update_minstake -r "[{\"vname\":\"min_stake\",\"type\":\"Uint128\",\"value\":\"10000000000\"}]" -f true
