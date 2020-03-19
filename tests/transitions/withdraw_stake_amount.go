44df37f5932a64d2effbde75751dad9f70aa6195
xiaohuo <xiaohuo200@gmail.com>
2020-03-19 18:43:50 +0800

    wip: test withdraw amount

--- tests/transitions/withdraw_stake_amount.go
+++ tests/transitions/withdraw_stake_amount.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	contract2 "github.com/Zilliqa/gozilliqa-sdk/contract"
+	"github.com/Zilliqa/gozilliqa-sdk/keytools"
+	"github.com/Zilliqa/gozilliqa-sdk/util"
 	"strconv"
 	"strings"
 )
@@ -20,7 +22,7 @@ func TestWithdrawAmount(pri1, pri2, api string) {
 	fmt.Println("impl = ", impl)
 	p := NewProxy(api, proxy, impl)
 
-	minstake := "100000000000"
+	minstake := "1000"
 
 	err = p.updateMinStake(pri1, minstake)
 	if err != nil {
@@ -37,16 +39,142 @@ func TestWithdrawAmount(pri1, pri2, api string) {
 		panic("update contract max stake failed: " + err.Error())
 	}
 
+	err2 := p.updateVerifier(pri1)
+	if err2 != nil {
+		panic("test withdraw amount failed: update verifier error: " + err2.Error())
+	}
+
 	// 1. no such ssn
-	err, event := p.withdrawAmount(pri1, "10000")
-	fmt.Println(err.Error())
-	fmt.Println(event)
+	err, event := p.withdrawAmount(pri2, minstake)
 	if event == "SSN doesn't exist" {
 		fmt.Println("test withdraw amount (no such ssn) succeed")
 	} else {
 		panic("test withdraw amount (no such ssn) failed: event error")
 	}
 
+	ssnaddr := "0x" + keytools.GetAddressFromPrivateKey(util.DecodeHex(pri2))
+	parameters := []contract2.Value{
+		{
+			VName: "ssnaddr",
+			Type:  "ByStr20",
+			Value: ssnaddr,
+		},
+		{
+			VName: "stake_amount",
+			Type:  "Uint128",
+			Value: "0",
+		},
+		{
+			VName: "rewards",
+			Type:  "Uint128",
+			Value: "0",
+		},
+		{
+			VName: "urlraw",
+			Type:  "String",
+			Value: "devapiziiliqacom",
+		},
+		{
+			VName: "urlapi",
+			Type:  "String",
+			Value: "ziiliqacom",
+		},
+		{
+			VName: "buffered_deposit",
+			Type:  "Uint128",
+			Value: "0",
+		},
+	}
+	args, _ := json.Marshal(parameters)
+	if err2, output := ExecZli("contract", "call",
+		"-k", pri1,
+		"-a", p.Addr,
+		"-t", "add_ssn",
+		"-f", "true",
+		"-r", string(args)); err2 != nil {
+		panic("call transaction error: " + err2.Error())
+	} else {
+		tx := strings.TrimSpace(strings.Split(output, "confirmed!")[1])
+		payload := p.Provider.GetTransaction(tx).Result.(map[string]interface{})
+		receipt := payload["receipt"].(map[string]interface{})
+		success := receipt["success"].(bool)
+		if success {
+			fmt.Println("test withdraw amount: add ssn succeed")
+		} else {
+			panic("test withdraw amount failed: add ssn failed")
+		}
+	}
+
+	// 2 stake deposit
+	m := p.Provider.GetBalance(p.ImplAddress).Result.(map[string]interface{})
+	r := m["balance"].(string)
+	old, err := strconv.ParseInt(r, 10, 64)
+	if err != nil {
+		panic("test withdraw amount failed: parse balance error: " + err.Error())
+	}
+
+	if err2, _ := ExecZli("contract", "call",
+		"-k", pri2,
+		"-a", proxy,
+		"-t", "stake_deposit",
+		"-f", "true",
+		"-m", minstake,
+		"-r", "[]"); err2 != nil {
+		panic("test withdraw amount failed: call transaction error: " + err2.Error())
+	} else {
+		m := p.Provider.GetBalance(p.ImplAddress).Result.(map[string]interface{})
+		r := m["balance"].(string)
+		newbalance, err := strconv.ParseInt(r, 10, 64)
+		if err != nil {
+			panic("test withdraw amount failed: parse balance error: " + err.Error())
+		}
+		delta := newbalance - old
+		d := strconv.FormatInt(delta, 10)
+		if d != minstake {
+			panic("test withdraw amount failed: check state failed")
+		} else {
+			fmt.Println("test withdraw amount failed: stake deposit succeed")
+		}
+	}
+
+	// 3. deposit again
+	m = p.Provider.GetBalance(p.ImplAddress).Result.(map[string]interface{})
+	r = m["balance"].(string)
+	old, err = strconv.ParseInt(r, 10, 64)
+	if err2, _ := ExecZli("contract", "call",
+		"-k", pri2,
+		"-a", proxy,
+		"-t", "stake_deposit",
+		"-f", "true",
+		"-m", minstake,
+		"-r", "[]"); err2 != nil {
+		panic("test withdraw amount failed: call transaction error: " + err2.Error())
+	} else {
+		m := p.Provider.GetBalance(p.ImplAddress).Result.(map[string]interface{})
+		r := m["balance"].(string)
+		newbalance, err := strconv.ParseInt(r, 10, 64)
+		if err != nil {
+			panic("test withdraw amount failed: parse balance error: " + err.Error())
+		}
+		delta := newbalance - old
+		d := strconv.FormatInt(delta, 10)
+		if d != minstake {
+			panic("test withdraw amount failed: check state failed")
+		} else {
+			fmt.Println("test withdraw amount failed: stake deposit succeed")
+		}
+	}
+
+
+	// 4. withdraw
+	err2, event2 := p.withdrawAmount(pri2, minstake)
+	fmt.Println(err2.Error())
+	fmt.Println(event2)
+	if event2 == "SSN withdrawal not allowed when some deposit is bufferred" {
+		fmt.Println("test withdraw amount (no such ssn) succeed")
+	} else {
+		panic("test withdraw amount (no such ssn) failed: event error")
+	}
 	fmt.Println("------------------------ end TestWithdrawAamount ------------------------")
 
 }
