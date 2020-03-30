9617b91a249792d3ca2de6b5cb1e4dd1fbbdf033
xiaohuo <xiaohuo200@gmail.com>
2020-03-30 18:34:19 +0800

    feat: draw contract balance

--- scripts/deploy_multisig_wallet.sh
+++ scripts/deploy_multisig_wallet.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-OWNER_KEY=40a08154418fcc0026e9f93f6ed16c6c6a499cbcda1335b581084f18105d1c7b
 zli contract deploy -c ../contracts/multisig_wallet.scilla -i wallet.json -k ${OWNER_KEY}
