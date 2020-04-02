7981d8e4915772713d40d282e6c95edf7055103a
renlulu <lulu@zilliqa.com>
2020-04-02 21:19:23 +0800

    fix: trim extra char

--- scripts/Golang-ZLI/Contract-Admin/multisig_submit_updateMinStake_transaction.sh
+++ scripts/Golang-ZLI/Contract-Admin/multisig_submit_updateMinStake_transaction.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-zli contract call -a ${MULTISIG_WALLET_ADDRESS} -t SubmitUpdateMinStakeTransaction -k ${OWNER_KEY} -r "[{\"vname\":\"proxyContract\",\"type\":\"ByStr20\",\"value\":\"${PROXY_CONTRACT}\"},{\"vname\":\"min_stake\",\"type\":\"Uint128\",\"value\":\"${MIN_STAKE}\"}]" -f true
+zli contract call -a ${MULTISIG_WALLET_ADDRESS} -t SubmitUpdateMinStakeTransaction -k ${OWNER_KEY} -r "[{\"vname\":\"proxyContract\",\"type\":\"ByStr20\",\"value\":\"${PROXY_CONTRACT}\"},{\"vname\":\"min_stake\",\"type\":\"Uint128\",\"value\":\"${MIN_STAKE}\"]" -f true
