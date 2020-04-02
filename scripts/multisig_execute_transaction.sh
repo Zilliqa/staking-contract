2308bf6dbfa4b83e6b3cb5fde39dfaa4ad2ae80a
bb111189 <junhao@zilliqa.com>
2020-04-02 20:36:42 +0800

    chore: folder reorg

--- scripts/multisig_execute_transaction.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-zli contract call -a ${MULTISIG_WALLET_ADDRESS} -t ExecuteTransaction -k ${OWNER_KEY} -r "[{\"vname\":\"transactionId\",\"type\":\"Uint32\",\"value\":\"${TRANSACTION_ID}\"}]" -f true
--- scripts/multisig_sign_transaction.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-zli contract call -a ${MULTISIG_WALLET_ADDRESS} -t SignTransaction -k ${OWNER_KEY} -r "[{\"vname\":\"transactionId\",\"type\":\"Uint32\",\"value\":\"${TRANSACTION_ID}\"}]" -f true
