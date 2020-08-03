806961570752b033963f0378d30941b4ee308eb2
xiaohuo <xiaohuo200@gmail.com>
2020-08-03 17:29:57 +0800

    feat(test): deploy ssnlist

--- tests/deploy/ssnlist.go
+++ tests/deploy/ssnlist.go
@@ -1,5 +1,62 @@
 package deploy
 
-type SSNList struct {
+import (
+	"errors"
+	"github.com/Zilliqa/gozilliqa-sdk/account"
+	contract2 "github.com/Zilliqa/gozilliqa-sdk/contract"
+	"github.com/Zilliqa/gozilliqa-sdk/core"
+	"github.com/Zilliqa/gozilliqa-sdk/keytools"
+	"github.com/Zilliqa/gozilliqa-sdk/util"
+	"io/ioutil"
+)
 
+type SSNList struct {
+	Code string
+	Init []core.ContractValue
+	Addr string
+}
+
+func NewSSNList(key string, proxy string) (*SSNList,error) {
+	code,_ := ioutil.ReadFile("./ssnlist.scilla")
+	adminAddr := keytools.GetAddressFromPrivateKey(util.DecodeHex(key))
+
+	init := []core.ContractValue {
+		{
+			VName: "_scilla_version",
+			Type:  "Uint32",
+			Value: "0",
+		}, {
+			VName: "init_admin",
+			Type:  "ByStr20",
+			Value: "0x" + adminAddr,
+		}, {
+			VName: "proxy_address",
+			Type:  "ByStr20",
+			Value: "0x" + proxy,
+		},
+	}
+
+	wallet := account.NewWallet()
+	wallet.AddByPrivateKey(key)
+
+	contract := contract2.Contract{
+		Code:     string(code),
+		Init:     init,
+		Signer:   wallet,
+	}
+
+	tx,err := contract.DeployTo("isolated")
+	if err != nil {
+		return nil,err
+	}
+	tx.Confirm(tx.ID, 1000, 10, contract.Provider)
+	if tx.Status == core.Confirmed {
+		return &SSNList{
+			Code: string(code),
+			Init: init,
+			Addr: tx.ContractAddress,
+		},nil
+	} else {
+		return nil,errors.New("deploy failed")
+	}
 }
--- tests/main/main.go
+++ tests/main/main.go
@@ -14,4 +14,14 @@ func main() {
 	}
 
 	fmt.Println("deploy proxy succeed, address = ",proxy.Addr)
+
+	// 2. deploy ssn list
+	ssnlist,err1 := deploy.NewSSNList("e40afdc148c8f169613ba1bb2f9b15186cff6e1f5ad50ddc42aae7e5d51042bb",proxy.Addr)
+	if err1 != nil {
+		fmt.Println("deploy ssnlist error = ",err1.Error())
+		return
+	}
+
+	fmt.Println("deploy proxy succeed, address = ",ssnlist.Addr)
+
 }
