ae983a009487a445f3bde2793d48352de9389bff
xiaohuo <xiaohuo200@gmail.com>
2020-08-05 18:49:52 +0800

    test: update admin

--- tests/transitions/testing.go
+++ tests/transitions/testing.go
@@ -8,7 +8,9 @@ import (
 )
 
 const key1 = "e40afdc148c8f169613ba1bb2f9b15186cff6e1f5ad50ddc42aae7e5d51042bb"
+const addr1 = "29cf16563fac1ad1596dfe6f333978fece9706ec"
 const key2 = "8732034b0c895564d966e3df6968205211c7a2f0140b77c9e13de10c1ce77873"
+const addr2 = "e2cd74983c7a3487af3a133a3bf4e7dd76f5d928"
 
 type Testing struct {
 }
---
+++ tests/transitions/update_admin.go
@@ -0,0 +1,44 @@
+package transitions
+
+import (
+	"github.com/Zilliqa/gozilliqa-sdk/core"
+	"log"
+)
+
+func (t *Testing) UpdateAdmin() {
+	t.LogStart("UpdateAdmin")
+	proxy, ssnlist := t.DeployAndUpgrade()
+
+	// as admin, set new admin to addr2
+	ssnlist.LogContractStateJson()
+	args := []core.ContractValue{{
+		"admin",
+		"ByStr20",
+		"0x" + addr2,
+	}}
+	tnx, err := proxy.Call("UpdateAdmin", args)
+	if err != nil {
+		t.LogError("UpdateAdmin", err)
+	}
+	receipt := t.GetReceiptString(tnx)
+	log.Println(receipt)
+	t.AssertContain(receipt, addr2)
+	ssnlist.LogContractStateJson()
+
+	// as non admin, try to set new admin to addr1
+	args = []core.ContractValue{{
+		"admin",
+		"ByStr20",
+		"0x" + addr1,
+	}}
+	tnx, err2 := proxy.Call("UpdateAdmin", args)
+	t.AssertError(err2)
+
+	receipt = t.GetReceiptString(tnx)
+	log.Println(receipt)
+	t.AssertContain(receipt,"Message [(_exception : (String \\\"Error\\\")) ; (code : (Int32 -3))]")
+	ssnlist.LogContractStateJson()
+
+	t.LogEnd("UpdateAdmin")
+
+}
