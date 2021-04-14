cab9945f9af4ff784e40a9962076b23fded9930e
teye <teye@zilliqa.com>
2021-04-14 12:09:32 +0800

    Revert "add disambigous ADT bug fix"
    This reverts commit 7dee16a4089d4b69141dd3660fbe214168490585.

--- tests/transitions/withdraw_stake_amount2.go
+++ tests/transitions/withdraw_stake_amount2.go
@@ -11,9 +11,9 @@ func (t *Testing) WithDrawStakeAmount2() {
 	min := "100000000000000"
 	min2 := "200000000000000"
 	//tenzil := "10000000000000"
-	ssn1 := "0x" + addr1
+	ssn1 := "0x"+addr1
 	delegMin := "50000"
-	proxy.UpdateStakingParameters(min, delegMin)
+	proxy.UpdateStakingParameters(min,delegMin)
 	// update verifier to addr1
 	proxy.UpdateVerifier(ssn1)
 	// update verifier receiving addr to addr2
@@ -24,21 +24,21 @@ func (t *Testing) WithDrawStakeAmount2() {
 	ssnlist.LogContractStateJson()
 	// add delegator (addr1) to ssn1 (addr1) with min zil, make ssn active
 	proxy.UpdateWallet(key1)
-	proxy.DelegateStake(ssn1, min)
+	proxy.DelegateStake(ssn1,min)
 	// delegate again, comes to buffered deposit
-	proxy.DelegateStake(ssn1, min)
+	proxy.DelegateStake(ssn1,min)
 	ssnlist.LogContractStateJson()
 
 	// try withdraw, should fail
-	txn, err := proxy.WithdrawStakeAmt("0x"+addr1, min2)
+	txn, err := proxy.WithdrawStakeAmt("0x" + addr1,min2)
 	t.AssertError(err)
 	t.LogPrettyReceipt(txn)
 
 	// reward
-	proxy.AssignStakeRewardFixed(ssn1, "52000000")
+	proxy.AssignStakeReward(ssn1, "52000000")
 
 	// try withdraw, should fail (because of rewards)
-	txn, err1 := proxy.WithdrawStakeAmt(ssn1, min2)
+	txn, err1 := proxy.WithdrawStakeAmt(ssn1,min2)
 	t.AssertError(err1)
 	t.LogPrettyReceipt(txn)
 
@@ -46,9 +46,9 @@ func (t *Testing) WithDrawStakeAmount2() {
 	proxy.WithdrawStakeRewards(ssn1)
 
 	// withdraw amount
-	txn, err2 := proxy.WithdrawStakeAmt(ssn1, min2)
+	txn, err2 := proxy.WithdrawStakeAmt(ssn1,min2)
 	if err2 != nil {
-		t.LogError("WithDrawStakeAmount2", err2)
+		t.LogError("WithDrawStakeAmount2",err2)
 	}
 	t.LogPrettyReceipt(txn)
 	t.LogEnd("WithDrawStakeAmount2")
